5c8167a16093e7061b165fff6852ff520813f9ae
oc <oc@rynning.no>
2008-10-21 13:00:04 +0200

    Deleted non-used methods

--- models/string_ext.rb
+++ models/string_ext.rb
@@ -4,12 +4,6 @@ class String
     RedCloth.new(self).to_html
   end
  
-  def auto_link
-    if self =~ /(\[((https?|ftp):[^'">\s|]+)(\|([^|'"]+))?)\]/xi
-      self.gsub($1, %Q{<a href="#{$2}">#{$5 || $1}</a>})
-    end
-  end
- 
   def wiki_link
     self.gsub(/\s+([A-Z][a-z]+[A-Z][A-Za-z0-9]+)\s+/) do |page|
       unless Page.by_name(page).empty?
@@ -20,11 +14,4 @@ class String
     end
   end
   
-  def titleize
-    self.gsub(/([A-Z]+)([A-Z][a-z])/,'\1 \2').gsub(/([a-z\d])([A-Z])/,'\1 \2')
-  end
- 
-  def without_ext
-    self.sub(File.extname(self), '')
-  end
 end
